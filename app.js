8f6a90633bfd3ae6a8f60053fff64bc7cf918b00
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-07-28 16:01:49 +0500

    fix: send JSON, when way wrong

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { sendNotFoundError,
+} = require('../utils/error');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -19,7 +21,7 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 app.use('/', (req, res) => {
-  res.status(404).send("wrong")
+  sendNotFoundError(res)
 })
 
 app.listen(PORT);
