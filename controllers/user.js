7fa781aa9d0c2e1998e07a9601729c9402d4930b
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-07-28 11:45:13 +0500

    fix: fix tests about update users

--- controllers/user.js
+++ controllers/user.js
@@ -42,7 +42,7 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, {runValidators: true})
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => {
       if (!user) {
         return sendNotFoundError(res);
